c9529a9c92c0a8a8a2ff341bd3e5c5843d69faa6
Adore03 <145483400+Adore03@users.noreply.github.com>
2023-09-19 22:41:44 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from PIL import Image
-
-# Load the background image
-background = Image.open(r'C:\Users\Abhishek\OneDrive\Desktop\pexels-nataliya-vaitkevich-7615574.jpg')
+import base64
+import plotly.express as px
 
 # Set the page configuration
 st.set_page_config(
@@ -12,6 +10,49 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+
+df = px.data.iris()
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64(r"C:\Users\Abhishek\OneDrive\Desktop\pexels-nataliya-vaitkevich-7615574.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img}");
+background-size: 100%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("https://wallpapercave.com/wp/wp6845532.jpg"); 
+background-repeat: no-repeat;
+background-size: 100%;
+background-attachment: fixed;
+background-position: right;
+font-color:Black;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.sidebar.header("Configuration")
+
 def main():
     st.title("Ayurvedic Practitioner's Portal")
 
@@ -20,7 +61,6 @@ def main():
 
     if choice == "Home":
         st.subheader("Home")
-        st.image(background)
 
     elif choice == "Login":
         st.subheader("Login Section")
