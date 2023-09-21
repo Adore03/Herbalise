8c73e4c58bbc66be05d7bdf545f6b892ccc83f01
Adore03 <145483400+Adore03@users.noreply.github.com>
2023-09-21 17:20:57 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -4,8 +4,11 @@ import plotly.express as px
 import streamlit_authenticator as stauth
 import sqlite3
 import yaml
+import os
+from transformers import RagTokenizer, RagRetriever, RagTokenForGeneration 
+from dotenv import load_dotenv
 from yaml.loader import SafeLoader
-with open(r'C:\Users\Abhishek\Desktop\credentials.yaml') as file:
+with open(r'C:\Users\Abhishek\Desktop\Herbalise\credentials.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Set the page configuration
@@ -15,6 +18,12 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
+
+load_dotenv()  # Load environment variables from .env file
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Get API key from environment variable
+tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
+retriever = RagRetriever.from_pretrained("facebook/rag-token-nq", index_name="exact", use_dummy_dataset=True)
+model = RagTokenForGeneration.from_pretrained("facebook/rag-token-nq", retriever=retriever)
 
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
@@ -28,10 +37,20 @@ def login_user(username,password):
     c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username,password))
     data = c.fetchall()
     return data
+
 def view_all_users():
-    c.execute('SELECT * FROM userstable')
+    c.execute('SELECT * FROM usertable')
     data = c.fetchall()
     return data
+
+def application():
+    authenticator.logout('Logout','main')
+    user_input = st.text_input("Enter your query:")
+    if st.button("Generate"):
+        inputs = tokenizer.prepare_seq2seq_batch([user_input], return_tensors="pt")
+        generated = model.generate(inputs["input_ids"])
+        st.write(tokenizer.batch_decode(generated, skip_special_tokens=True))
+
 
 
 authenticator = stauth.Authenticate (
@@ -41,8 +60,6 @@ authenticator = stauth.Authenticate (
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-
-
 
 
 df = px.data.iris()
@@ -85,12 +102,12 @@ right: 2rem;
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.sidebar.header("Configuration")
+st.sidebar.title("Configuration")
 
 def main():
     st.title("Ayurvedic Practitioner's Portal")
 
-    menu = ["Home", "Login", "SignUp"]
+    menu = ["Home", "Login", "SignUp","Application"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
@@ -98,32 +115,27 @@ def main():
 
     elif choice == "Login":
         st.subheader("Login Section")
-        name, authentication_status, username = authenticator.login('Login', 'main')
-        if authentication_status:
-            authenticator.logout('Logout', 'main')
-        if username == 'tswift':
-            st.write(f'Welcome *{name}*')
-            authenticator.logout('Logout', 'main')
-            st.title('Application 1')
-
-        elif username == 'weeknd':
-            st.write(f'Welcome *{name}*')
-            authenticator.logout('Logout', 'main')
-            st.title('Application 2')
-            
-        elif authentication_status == False:
-            st.error('Username/password is incorrect')
-        elif authentication_status == None:
-            st.warning('Please enter your username and password')
-
+        username_input = st.text_input("Username")
+        password_input = st.text_input("Password", type='password')
+        if st.button("Login"):
+            data=login_user(username_input, password_input)
+            print("Data from the database:", data)
+            if data:
+                st.write(f'Welcome *{data[0][0]}*')
+                application()
+            else:
+                st.error('username/password is incorrect')
 
     elif choice == "SignUp":
-        st.subheader("Create a New Account")
-        new_user = st.text_input("Username")
-        new_password = st.text_input("Password", type='password')
-        if st.button("Signup"):
-            # Implement your account creation logic here
-            st.success("You have successfully created an account")
+        st.subheader("Create an Account")
+        username = st.text_input('Username')
+        password = st.text_input('Password',type='password')
+        if st.button('SignUp'):
+            create_usertable()
+            add_userdata(username,password)
+            st.success("You have successfully created an account. Go to the Login Menu to login")
+            view_all_users
+    
             
 if __name__ == "__main__":
     main()
